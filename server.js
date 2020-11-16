033a07ea49694dc9b6cc52f3ed0499cd858050a3
tianyu5177 <zhangtianyu5177@sina.com>
2020-11-16 11:40:23 +0800

    更新了一些代码

--- server.js
+++ server.js
@@ -11,19 +11,19 @@ app.use(express.json())
 //暴露静态资源
 app.use(express.static(__dirname+'/src'))
 
-//响应GET请求---可以接收query参数
+//响应GET请求--可以接收query参数
 app.get('/test_get',(request,response)=>{
 	console.log('有人请求test_get了--携带的query参数是：',request.query);
 	response.send('hello_test_get')
 })
 
-//响应GET请求----可以接收params参数
+//响应GET请求--可以接收params参数
 app.get('/test_get2/:name/:age',(request,response)=>{
 	console.log('有人请求test_get2了--携带的params参数是：',request.params);
 	response.send('hello_test_get2')
 })
 
-//响应POST请求----可以接收请求体参数
+//响应POST请求--可以接收请求体参数
 app.post('/test_post',(request,response)=>{
 	console.log('有人请求test_post了--携带的请求体参数是',request.body);
 	response.send('hello_test_post')
